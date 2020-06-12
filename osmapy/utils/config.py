5e8652a7663e40f3d876bd817dac7f284bfb69eb
theFeiter <git@marvinfeiter.de>
2020-06-12 13:57:35 +0200

    Add validation of config file

--- osmapy/utils/config.py
+++ osmapy/utils/config.py
@@ -5,19 +5,33 @@ configurations are hardcoded here.
 """
 
 import pathlib
-
 import yaml
+
 from easydict import EasyDict
+from cerberus import Validator
 
 path_base = pathlib.Path(__file__).parent
 path_config = path_base / pathlib.Path("../config.txt")
+path_schema = path_base / pathlib.Path("schema.py")
 
-with open(path_config, "r") as file:
+
+
+with  open(path_config, "r") as file:
+    schema = eval(open(path_schema, 'r').read())
+    v = Validator(schema)
     try:
-        config = EasyDict(yaml.safe_load(file))
+        doc = yaml.safe_load(file)
     except:
         print("Something went wrong while loading your config file. Please check it for format errors.")
         exit()
+
+    if not v.validate(doc, schema):
+        print("There are errors in your configuration file.")
+        print (v.errors)
+        exit()
+
+    config = EasyDict(doc)
+         
 
 config.image_size = 256     # tile size
 config.retry_time_tile = 4  # Wait 4 seconds before retry to load a slippy tile
